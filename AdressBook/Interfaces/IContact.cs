4a6842eafc42439e174a6037f2f809afebc2f16b
felyc <fely.coman@gmail.com>
2023-12-26 11:12:55 +0100

    deleted id, PersonNumber and isActive from Icontacts

--- AdressBook/Interfaces/IContact.cs
+++ AdressBook/Interfaces/IContact.cs
@@ -4,14 +4,13 @@ namespace AdressBook.Interfaces;
 
 public interface IContact
 {
-    int Id { get; set; }
+ 
     string FirstName { get; set; }
     string LastName { get; set; }
     string Email { get; set; }
     string? PhoneNumber { get; set; }
     string Adress { get; set; }
-    Guid PersonNumber { get; set; }
-    bool IsActive { get; set; }
+ 
 
 
 }
