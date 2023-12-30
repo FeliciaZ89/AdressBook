ddd1081c28679ea4efa2851940cc59a3a542cb17
felyc <fely.coman@gmail.com>
2023-12-30 09:38:41 +0100

    modified FileServiceTests.cs/added one more test

--- AdressBook.Tests/FileServiceTest.cs
+++ AdressBook.Tests/FileServiceTest.cs
@@ -22,17 +22,50 @@ public class FileServiceTest
     }
 
     [Fact]
-    public void GetContentFromFileShould_ReturnTrue_IfTheFileExists()
+    public void SaveContentToFileShould_ReturnFalse_IfFilePathDoNotExists()
     {
         // Arrange
         IFileService fileService = new FileService();
-        string filePath = @"C:\Agenda\AdressBook\testcontent.txt";
+        string filePath = @"C:\Agenda\AdressBook\Test\testcontent.txt";
         string content1 = "Test content";
 
         // Act
         bool result = fileService.SaveContentToFile(filePath, content1);
 
         // Assert
-        Assert.True(result);
+        Assert.False(result);
     }
+
+    [Fact]
+    public void GetContentFromFileShould_ReturnFileContent_WhenFileExists()
+    {
+        // Arrange
+        IFileService fileService = new FileService();
+        string filePath = @"C:\Agenda\AdressBook\testcontent.txt";
+        string expectedContent = "Test content";
+
+        // Create the file with test content
+        File.WriteAllText(filePath, expectedContent);
+
+        // Act
+        string result = fileService.GetContentFromFile(filePath);
+
+        // Assert
+        Assert.Equal(expectedContent, result);
+    }
+
+    [Fact]
+    public void GetContentFromFileShould_ReturnNull_WhenFileDoesNotExist()
+    {
+        // Arrange
+        IFileService fileService = new FileService();
+        string filePath = @"C:\Agenda\AdressBook\Test\testcontent.txt";
+
+        // Act
+        string result = fileService.GetContentFromFile(filePath);
+
+        // Assert
+        Assert.Null(result);
+    }
+
 }
